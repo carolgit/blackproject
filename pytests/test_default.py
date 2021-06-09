12d7a30bf19f5446f02670a1b97703541b03cf40
Ana Caroline Ferreira <ana.caroline.ferreir@gmail.com>
2021-06-09 14:29:57 -0300

    gitignore

--- pytests/test_default.py
+++ pytests/test_default.py
@@ -1,7 +1,51 @@
 import pytest
+from django.contrib.auth.models import User
 
-def test_always_passes():
-    assert True
+from django.urls import reverse
 
-def test_always_fails():
-    assert False
+@pytest.mark.django_db
+def test_user_create():
+  User.objects.create_user('john', 'john@doe.com', 'johnpassword')
+  assert User.objects.count() == 1
+
+# @pytest.mark.django_db 
+# def test_view(client):
+#    url = reverse('homepage-url')
+#    response = client.get(url)
+#    assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_unauthorized(client):
+   url = reverse('adm-main')
+   response = client.get(url)
+   assert response.status_code == 401
+
+# @pytest.mark.django_db
+# def test_superuser_view(admin_client):
+#    url = reverse('superuser-url')
+#    response = admin_client.get(url)
+#    assert response.status_code == 200
+
+# @pytest.mark.django_db
+# def test_user_detail(client, django_user_model):
+#    user = django_user_model.objects.create(
+#        username='someone', password='password'
+#    )
+#    url = reverse('user-detail-view', kwargs={'pk': user.pk})
+#    response = client.get(url)
+#    assert response.status_code == 200
+#    assert 'someone' in response.content
+
+# @pytest.mark.django_db
+# @pytest.mark.parametrize([
+#    ('gr', 'Yasou'),
+#    ('de', 'Guten tag'),
+#    ('fr', 'Bonjour')
+# ])
+# def test_languages(language_code, text, client):
+#    url = reverse('say-hello-url')
+#    response = client.get(
+#        url, data={'language_code': language_code}
+#    )
+#    assert response.status_code == 200
+#    assert text == response.content
