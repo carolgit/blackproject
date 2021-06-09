e360cfbd3c995bbf8293264029c3cf49c9fdc84b
Ana Caroline Ferreira <ana.caroline.ferreir@gmail.com>
2021-06-09 15:40:30 -0300

    testes parte 1

--- pytests/test_default.py
+++ pytests/test_default.py
@@ -1,5 +1,6 @@
 import pytest
 from django.contrib.auth.models import User
+from about.models import Post
 
 from django.urls import reverse
 
@@ -8,17 +9,25 @@ def test_user_create():
   User.objects.create_user('john', 'john@doe.com', 'johnpassword')
   assert User.objects.count() == 1
 
-# @pytest.mark.django_db 
-# def test_view(client):
-#    url = reverse('homepage-url')
-#    response = client.get(url)
-#    assert response.status_code == 200
-
 @pytest.mark.django_db
-def test_unauthorized(client):
-   url = reverse('adm-main')
-   response = client.get(url)
-   assert response.status_code == 401
+def test_post_create():
+  User.objects.create_user('john', 'john@doe.com', 'johnpassword')
+  user = User.objects.all()[:1].get()
+  post = Post(author=user, title='Hello Word', text='dldkjbafas lkfsd falhdds adf a')
+  post.save()
+  assert Post.objects.count() == 1
+
+@pytest.mark.django_db 
+def test_view(client):
+   url = reverse('post_content')
+   response = client.get(url+'/1')
+   assert response.status_code == 404
+
+# @pytest.mark.django_db
+# def test_unauthorized(client):
+#    url = reverse('adm-main')
+#    response = client.get(url)
+#    assert response.status_code == 401
 
 # @pytest.mark.django_db
 # def test_superuser_view(admin_client):
